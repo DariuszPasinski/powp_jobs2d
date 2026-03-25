4a38333bed6e1d416829e2d253c5a7985a4bbcb1
DariuszPasiński <245897@edu.p.lodz.pl>
2026-03-25 12:53:46 +0100

    Implementacji hierarchi klas reprezentujących podstawowe funkcje urządzenoa, wersja niesztywna.

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/OperateToCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.drivers;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    public OperateToCommand(Job2dDriver driver, int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver d) {
+        d.operateTo(x, y);
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/SetPositionCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.drivers;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    public SetPositionCommand(Job2dDriver driver, int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver d){
+        d.setPosition(x, y);
+    }
+}
