073e31af815098e67588bb825f42bd188893b4c6
DariuszPasiński <245897@edu.p.lodz.pl>
2026-03-25 13:25:52 +0100

    Dodanie klasy ComplexCommand, która umożliwia reprezentowanie dowolnego ciągu poleceń.

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/ComplexCommand.java
@@ -0,0 +1,19 @@
+package edu.kis.powp.jobs2d.drivers;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+    private List <DriverCommand> listOfCommands;
+
+    public void add(DriverCommand command) {
+        listOfCommands.add(command);
+    }
+
+    public void execute(Job2dDriver driver){
+        for(DriverCommand command: listOfCommands) {
+            command.execute(driver);
+        }
+    }
+}
